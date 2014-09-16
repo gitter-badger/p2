27b0d11fcb17910a148f33f5a474012af1435cfb
Jose Andrade <jandrade@gmail.com>
2014-09-16 16:09:34 -0400

    Removed unneeded files

--- testform.php
+++
@@ -1,7 +0,0 @@
-<form name="form" action="test.php" method="post">
-   <input type="checkbox" name="animalsCheck" value="yes" checked />Animals<br />
-   <input type="checkbox" name="clothesCheck" value="yes" checked />Clothes<br />
-   <input type="checkbox" name="fruitsCheck" value="yes" checked />Fruits<br />
-   <input type="checkbox" name="toolsCheck" value="yes" checked />Tools<br />
-   <input type="submit" value="SUBMIT" />
-</form>
